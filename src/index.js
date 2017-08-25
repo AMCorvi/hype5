b99fe8a03e22bf37964a33a2d8ad6ad7364a857a
A M Corvi <amcorvi@icloud.com>
2017-08-24 22:07:08 -0400

    feat(API Methods): Added new API methods
    Added new API method which return top or latest track that are note remix. And another method that
    return methods list of tracks in generally.
    
    closes #3

--- src/index.js
+++ src/index.js
@@ -30,7 +30,88 @@ async function runCasper(type, filter) {
 	return returnValue;
 }
 
-export const noremixes = () => {};
+
+export const top = async (type, crawlFunc = runCasper) => {
+	// list of options that are actually valid
+	const options = ["popular", "latest"];
+	// Value assigned to and return by function
+	let output;
+
+	// Compare filter parameter with list of possible valid filters
+	try {
+		if (!options.find(val => val === type)) {
+			throw new SyntaxError(
+				` \n\n \t Call of function 'remixes' made with invalid arg ${type}.\n\t Insert valid filter argument. \n\t e.g. ${options}\n`,
+				"index.js",
+				89
+			);
+		}
+	} catch (e) {
+		throw e;
+	}
+
+	const getData = () => {
+		return new Promise((resolve, reject) => {
+			try {
+				const x = crawlFunc(type);
+				resolve(x);
+			} catch (err) {
+				reject(err);
+			}
+		});
+	};
+
+	await getData()
+		.then(data => {
+			output = data;
+		})
+		.catch(err => {
+			throw err;
+		});
+
+	return output;
+};
+
+export const noremixes = async (type, crawlFunc = runCasper) => {
+	// list of options that are actually valid
+	const options = ["popular", "latest"];
+	// Value assigned to and return by function
+	let output;
+
+	// Compare filter parameter with list of possible valid filters
+	try {
+		if (!options.find(val => val === type)) {
+			throw new SyntaxError(
+				` \n\n \t Call of function 'remixes' made with invalid arg ${type}.\n\t Insert valid filter argument. \n\t e.g. ${options}\n`,
+				"index.js",
+				89
+			);
+		}
+	} catch (e) {
+		throw e;
+	}
+
+	const getData = () => {
+		return new Promise((resolve, reject) => {
+			try {
+				const x = crawlFunc(type, "noremix");
+				resolve(x);
+			} catch (err) {
+				reject(err);
+			}
+		});
+	};
+
+	await getData()
+		.then(data => {
+			output = data;
+		})
+		.catch(err => {
+			throw err;
+		});
+
+	return output;
+};
 
 export const remixes = async (type, crawlFunc = runCasper) => {
 	// list of options that are actually valid
--- src/index.test.js
+++ src/index.test.js
@@ -2,23 +2,34 @@ const hype5 = require("./index.js");
 
 describe("hype5", () => {
 	describe("remix method", () => {
-		test.skip("That method checks for vali type parameter", done => {
+
+		test("Method is instance of promise", () => {
+			let crawlFuncSim = jest.fn((a, b) => a + b);
+			let rem = hype5.remixes;
+			expect(rem("popular", crawlFuncSim)).toBeInstanceOf(Promise);
+		});
+
+		test("Method is return a STRING", () => {
+			let crawlFuncSim = jest.fn((a, b) => a + b);
+			let rem = hype5.remixes;
+			expect(rem("popular", crawlFuncSim)).resolves.toBeInstanceOf(String);
+			expect(rem("popular", crawlFuncSim)).resolves.toBe("popularremix");
+		});
+
+		test("Method checks for valid type parameter", done => {
 			hype5.remixes("cool").catch(err => {
 				expect(err).toBeInstanceOf(Error);
 				done();
 			});
 		});
 
-    test("Method return 'Promise'")
-
-		test("That method calls crawlFunc function", async () => {
-			let crawlFuncSim = jest.fn((a, b) => `${a} ${b}`);
+		test("Method calls crawlFunc function", async () => {
+			let crawlFuncSim = jest.fn((a, b) => a + b);
 			let rem = hype5.remixes;
 
-
 			rem = await rem("popular", crawlFuncSim);
-      expect(crawlFuncSim.mock.calls[0][1]).toBe("remix");
-      expect(crawlFuncSim.mock.calls[0][0]).toBe("popular");
+			expect(crawlFuncSim.mock.calls[0][1]).toBe("remix");
+			expect(crawlFuncSim.mock.calls[0][0]).toBe("popular");
 		});
 	});
 });
