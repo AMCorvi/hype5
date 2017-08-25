2a698d7af5c772a2ff3e0081844c117b3d03acaf
A M Corvi <amcorvi@icloud.com>
2017-08-24 22:31:04 -0400

    test(tests): Added test for new methods '.top' and '.noremixes'

--- src/index.test.js
+++ src/index.test.js
@@ -1,15 +1,79 @@
 const hype5 = require("./index.js");
 
 describe("hype5", () => {
-	describe("remix method", () => {
+	describe("'top' method", () => {
 
-		test("Method is instance of promise", () => {
+		test("Method is instance of Promise", () => {
+			let crawlFuncSim = jest.fn((a) => a );
+			let rem = hype5.top;
+			expect(rem("popular", crawlFuncSim)).toBeInstanceOf(Promise);
+		});
+
+		test("Method is returns a STRING", async () => {
+			let crawlFuncSim = jest.fn((a) => a );
+			let rem = hype5.top;
+			await expect(rem("latest", crawlFuncSim)).resolves.toBe("latest");
+		});
+
+		test("Method checks for valid type parameter", done => {
+			hype5.top("cool").catch(err => {
+				expect(err).toBeInstanceOf(Error);
+				done();
+			});
+		});
+
+		test("Method calls crawlFunc function", async () => {
+			let crawlFuncSim = jest.fn((a) => a );
+			let rem = hype5.top;
+
+			rem = await rem("latest", crawlFuncSim);
+			expect(crawlFuncSim.mock.calls[0][1]).toBeUndefined();
+			expect(crawlFuncSim.mock.calls[0][0]).toBe("latest");
+		});
+
+	});
+
+	describe("'noremixes' method", () => {
+
+		test("Method is instance of Promise", () => {
+			let crawlFuncSim = jest.fn((a, b) => a + b);
+			let rem = hype5.noremixes;
+			expect(rem("popular", crawlFuncSim)).toBeInstanceOf(Promise);
+		});
+
+		test("Method is returns a STRING", () => {
+			let crawlFuncSim = jest.fn((a, b) => a + b);
+			let rem = hype5.noremixes;
+			expect(rem("popular", crawlFuncSim)).resolves.toBeInstanceOf(String);
+			expect(rem("popular", crawlFuncSim)).resolves.toBe("popularremix");
+		});
+
+		test("Method checks for valid type parameter", done => {
+			hype5.noremixes("cool").catch(err => {
+				expect(err).toBeInstanceOf(Error);
+				done();
+			});
+		});
+
+		test("Method calls crawlFunc function", async () => {
+			let crawlFuncSim = jest.fn((a, b) => a + b);
+			let rem = hype5.noremixes;
+
+			rem = await rem("popular", crawlFuncSim);
+			expect(crawlFuncSim.mock.calls[0][1]).toBe("noremix");
+			expect(crawlFuncSim.mock.calls[0][0]).toBe("popular");
+		});
+	});
+
+	describe("'remix' method", () => {
+
+		test("Method is instance of Promise", () => {
 			let crawlFuncSim = jest.fn((a, b) => a + b);
 			let rem = hype5.remixes;
 			expect(rem("popular", crawlFuncSim)).toBeInstanceOf(Promise);
 		});
 
-		test("Method is return a STRING", () => {
+		test("Method is returns a STRING", () => {
 			let crawlFuncSim = jest.fn((a, b) => a + b);
 			let rem = hype5.remixes;
 			expect(rem("popular", crawlFuncSim)).resolves.toBeInstanceOf(String);
